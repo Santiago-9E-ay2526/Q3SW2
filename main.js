9322a645bb60ff86b72b02a7da82a66af8ec1949
Santiago <santiago.mj@obmontessori.edu.ph>
2026-02-20 11:26:24 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+// Function for Discount Button
+function discountButton (){
+     let firstBudget = document.getElementById(laptopbudget).value;
+     let discountPrice = Number(firstBudget * 0.2);
+
+     document.getElementById().innerHTML =;
+}
